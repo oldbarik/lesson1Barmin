a03da543f363875983232be10e8952882bfe2d09
barik <barik.rar@gmail.com>
2021-07-31 21:06:59 +0400

    lesson2

--- src/main/java/lesson2/MethodApp/homework2Barmin.java
+++ src/main/java/lesson2/MethodApp/homework2Barmin.java
@@ -5,9 +5,9 @@ public class homework2Barmin {
     public static void main(String[] args) {
 
         System.out.println(sumMethod(10,5));
-        celNumber(3);
-        System.out.println(celNumberBool(0));
-        argNum ("любое слово" + 5);
+        celNumber(-5);
+        System.out.println(isNegative(6));
+        //argNum ("любое слово" + 5);
 
     }
 //Задание № 1 Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
@@ -22,25 +22,25 @@ public class homework2Barmin {
 
     public static void celNumber (int a){
         if (a >=0) {
-            System.out.println("Число " + a + "Положительное");
+            System.out.println("Число " + a + " Положительное");
         }else{
-            System.out.println("Число " + a + "Положительное");
+            System.out.println("Число " + a + " Отрицательное");
         }
     // Задание № 3 Написать метод, которому в качестве параметра передается целое число.
         // Метод должен вернуть true, если число отрицательное, и вернуть false если положительное
 
-        public static boolean celNumberBool(int a) {
-            return (a < 0);
+        public static boolean isNegative(int x) {
+            return (x < 0);
         }
-        //Задание № 4 Написать метод, которому в качестве аргументов передается строка и число,
-        // метод должен отпечатать в консоль указанную строку, указанное количество раз
 
-        public static void argNum (String num, int a) {
-            for (int i = 0; i < a; i++){
-                System.out.println (num);
-            }
+            //Задание № 4 Написать метод, которому в качестве аргументов передается строка и число,
+        // метод должен отпечатать в консоль указанную строку, указанное количество раз
+public static void argNum(String str, int a) {
+            for (int i = 0; i < a, i++);
+            System.out.println(str);
+        }
+
                 // с заданием 5 еще не разобрался
         }
 
     }
-}
