2d9f322dd41d50b35ce750324c0914e7df75e47f
barik <barik.rar@gmail.com>
2021-08-03 12:54:34 +0400

    lessons

---
+++ src/main/java/lesson1/lesson1.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class lesson1 {
+    public static void main(String[] args) {
+    }
+}
+
+
---
+++ src/main/java/lesson2/lesson2.java
@@ -0,0 +1,7 @@
+package lesson2;
+
+public class lesson2 {
+    public static void main(String[] args) {
+        System.out.println("все написано в заметках");
+    }
+}
